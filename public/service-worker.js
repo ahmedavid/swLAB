62088fa0ac33440cce0cc9c9a8e5cc0a2a92babe
David Ahmadov <david.ahmadov@samsa-it.de>
2017-07-31 18:07:41 +0200

    hep problem

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,22 +4,27 @@ self.addEventListener('install',function (event) {
 
 // self.addEventListener('fetch',function (event) {
 //     console.log("INTERCEPTING")
+//     var url  = event.request.url.replace(/^https:\/\//i, 'http://');
+//     console.log('URL:',url)
 //     event.waitUntil(
-//         fetch(event.request)
+//         fetch(url)
 //             .then(function (response) {
-//                 console.log("DONE:",response)
+//                 console.log("DONE SW:",response)
 //                 return response
 //             })
 //             .catch(function (err) {
-//                 console.error(err)
+//                 console.error('ERROR IN FETCH',err)
 //             })
 //     )
 // })
 
 self.addEventListener('fetch', function(event) {
+    console.log('INTERCEPTING')
     event.respondWith(
         caches.open('cachTEST').then(function(cache) {
             return cache.match(event.request).then(function (response) {
+                //event.request = event.request.replace(/^https:\/\//i, 'http://');
+                console.log('URL:',event.request)
                 return response || fetch(event.request).then(function(response) {
                         cache.put(event.request, response.clone());
                         return response;
